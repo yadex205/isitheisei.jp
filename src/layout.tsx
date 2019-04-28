b20f9fb34816e4fbd797de347f63938b91f94767
Kanon Kakuno <yadex205@outlook.jp>
2019-04-28 23:42:14 +0900

    :+1: Fix metas.

--- src/layout.tsx
+++ src/layout.tsx
@@ -13,13 +13,13 @@ export default class Layout extends React.Component<Props> {
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-          <link href="https://twitter.com/yadex205_vj" rel="author" />
+          <link href="/humans.txt" rel="author" />
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:creator" content="@yadex205_vj" />
           <meta property="og:title" content="Is it Heisei?" />
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://isitheisei.jp" />
-          <meta property="Is it Heisei?" />
+          <link href="https://isitheisei.jp" rel="canonical" />
           <title>Is it Heisei?</title>
         </Helmet>
         {this.props.children}
